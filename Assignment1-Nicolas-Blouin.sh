89dd4d40c826998410b4b60bbc46ab7a655c7222
Nicolas <n.blouin37@gmail.com>
2023-06-08 07:24:41 -0400

    Assignment 1 - Sysytem Status Almost complete

--- Assignment1-Nicolas-Blouin.sh
+++ Assignment1-Nicolas-Blouin.sh
@@ -11,7 +11,7 @@
 #Top of the script variables, I am storing the current USER on the system into
 #a variable called my_username for future use. I also am storing the current date
 #with a few options such as %A which will give me the full weekday name.
-sudo
+
 username=$USER
 date=$(date +"%A, %B %d, %Y %T")
 
@@ -32,6 +32,11 @@ host_ip_address=$(hostname -I | awk '{print $1}')
 default_gateway=$(ip r | grep "default via" | awk '{print $3}')
 dns_server=$(cat /etc/resolv.conf | grep "nameserver" | awk '{print $2}')
 
+list_of_users=$(who -u | awk '{print $1}' | tr '\n' ',')
+free_disk_space=$(df -h | awk '{print $1, $4}' | column -t)
+number_of_processes=$(ps -e --no-header | wc -l)
+load_averages=$(uptime | grep "load average: " | awk '{print $8, $9, $10}')
+memory_allocation=$(free -h -t)
 
 cat << EOF
 
@@ -98,5 +103,21 @@ DNS Server: $dns_server
 
 ====================================================
 
+System Status:
+--------------
+
+Users Logged In: $list_of_users
+
+Free Disk Space:
+
+$free_disk_space
+
+Processes Running (#): $number_of_processes
+
+Load Averages (1 Min, 5 Min, 15 Min): $load_averages
+
+Memory Allocation: 
+
+$memory_allocation
 
 EOF

3b4151a1849d2de0f2f143a8a269935dc28b364e
NBlouin367 <n.blouin37@gmail.com>
2023-08-02 15:30:40 -0400

    Assignment3 target 1 changes - Nicolas Blouin

--- Assignment3-Nicolas-Blouin.sh
+++ Assignment3-Nicolas-Blouin.sh
@@ -67,13 +67,127 @@ dpkg -s ufw &> /dev/null
 
 if [ $? -ne 0 ]; then
 
-    echo "UFW was not installed already."
+    echo "UFW is not installed."
     echo "Going to install UFW"
     apt-get install -y ufw > /dev/null
 
 
+    if [ $? -eq 0  ]; then
+
+        echo "Successfull installed UFW"
+
+    else
+
+        echo "UFW Install failed! Terminating the script"
+        exit 1
+
+    fi
+
+else
+
+    echo "UFW is already installed."
+
 fi
 
+#this if statement is running the ufw status command. Once it runs it is piping the output to the grep command
+#to search for the pattern of Status: active. By using -w the grep command will do a whole word search.
+#if the phrase Status: active appears in the ufw status output then the if block runs.
+
+if [[ $(ufw status | grep -w "Status: active") ]]; then
+
+     echo "UFW firewall status already active."
+     echo "Adding firewall rules."
+
+     #Will add rules anyways even if the firewall is active
+     #Setting all the ports I want to allow in the firewall configuration.
+
+     ufw allow from 172.16.1.0/24 to any port 514/udp
+
+     echo "Restarting/Reloading the firewall"
+
+     #restarting the firewall to apply the new changes using the ufw reload command.
+
+     ufw reload
+
+     #Using an if I can evaluate if the exit status of the ufw reload command was a 0 meaning a success.
+     #I then display a success message
+
+     if [ $? -eq 0 ]; then
+
+         echo "Firewall was successfully Restarted/Reloaded."
+
+     #When the above if statement is not ran then that would mean an exit status other than 0 occured.
+     #My else statement will then execute displaying an error and I terminate the script with an exit 1.
+
+     else
+
+         echo "Firewall could NOT restart. Terminating script."
+         exit 1
+
+     fi
+
+#when the firewall is not on run the else and enable it and apply rules to allow my listed ports
+
+else
+
+    echo "Turning on UFW firewall."
+
+    #turn on the firewall using ufw enable command
+
+    ufw enable
+
+    #If the exit status of my previous ufw enable command was 0 meaning success then run this if statement.
+    #I display a success message.
+
+    if [ $? -eq 0 ]; then
+
+        echo "Firewall was turned on successfully."
+
+    #When the previous if statement does not execute that would mean that the last command failed. This command being ufw enable
+    #I then use an else to handle the error displaying a message and terminating the script using exit 1.
+
+    else
+
+        echo "Firewall failed to enable and turn on. Stopping the script"
+        exit 1
+
+    fi
+
+    echo "Adding a few tcp firewall rules."
+
+    ufw allow from 172.16.1.0/24 to any port 514/udp
+
+    echo "Restarting firewall"
+
+    #restart the firewall to apply my settings using the ufw reload command
+
+    ufw reload
+
+    #Using an if to check if the exit status of my ufw reload command was a 0 meaning success. If a 0 then run this if statement.
+
+    if [ $? -eq 0 ]; then
+
+        echo "Firewall restarted successfully."
+
+    #When the above if statement is not executed then the exit status of ufw reload was something other than a 0.
+    #My else statement will then execute displaying an error and I terminate the script with an exit 1.
+
+    else
+
+        echo "Firewall could NOT restart properly. Terminating script"
+        exit 1
+
+    fi
+
+
+    echo "Firewall turned on and setup was successful!"
+
+fi
+
+#Rsyslog stuff
+
+sed -i 's/#module(load="imudp")/module(load="imudp")/' /etc/rsyslog.conf
+sed -i 's/#input(type="imudp" port="514")/input(type="imudp" port="514")/' /etc/rsyslog.conf
 
 EOF
 

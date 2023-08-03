987c6eb1e98131f2ba22d2e9a9cde843bf0c6bd9
NBlouin367 <n.blouin37@gmail.com>
2023-08-03 01:40:50 -0400

    SSH typo fix

--- Assignment3-Nicolas-Blouin.sh
+++ Assignment3-Nicolas-Blouin.sh
@@ -560,7 +560,7 @@ fi
 
 # start of NMS Section
 
-if ssh ssh -o StrictHostKeyChecking=no "$target2_management" << EOF
+if ssh -o StrictHostKeyChecking=no "$target2_management" << EOF
 
    systemctl is-active apache2 | grep -q "active"
 
@@ -589,7 +589,7 @@ else
 fi
 
 
-if ssh ssh -o StrictHostKeyChecking=no "$target1_management" << EOF
+if ssh -o StrictHostKeyChecking=no "$target1_management" << EOF
 
    systemctl is-active rsyslog | grep -q "active"
 

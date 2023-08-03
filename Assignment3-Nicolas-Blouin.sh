eca485ed1e85144fe79af21911f0d89c84942e0b
NBlouin367 <n.blouin37@gmail.com>
2023-08-03 02:33:48 -0400

    SSH fixes maybe

--- Assignment3-Nicolas-Blouin.sh
+++ Assignment3-Nicolas-Blouin.sh
@@ -560,7 +560,7 @@ fi
 
 # start of NMS Section
 
-if ssh -T -o StrictHostKeyChecking=no "$target2_management" << EOF
+if ssh -o StrictHostKeyChecking=no "$target2_management" 'cat' << EOF
 
    systemctl is-active apache2 | grep -q "active"
 
@@ -589,7 +589,7 @@ else
 fi
 
 
-if ssh -T -o StrictHostKeyChecking=no "$target1_management" << EOF
+if ssh -o StrictHostKeyChecking=no "$target1_management" 'cat' << EOF
 
    systemctl is-active rsyslog | grep -q "active"
 
